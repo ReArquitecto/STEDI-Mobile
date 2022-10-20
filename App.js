963b0b1cf497a99cda20c272b9ddfe114c52f79e
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2022-10-20 14:59:07 -0600

    Am I invited?

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
+import { Button, TextInput } from 'react-native-paper';
 
 
 
@@ -13,15 +14,32 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
 
    if (isFirstLaunch == true){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
- 
-);
-  }else{
-    return <Navigation/>
+
+  );
+} else if(isLoggedIn){
+  return <Navigation/>
+} else{
+  return(
+    <view>
+    <TextInput style={StyleSheet.input}
+    placeholderTextColor='#4251f5'
+    placeholder='Phone Number'>
+    </TextInput>
+    <Button
+    title='Send'
+      style={StyleSheet.button}
+      onPress={()=>{
+        console.log('Button was pressed!')
+      }}
+     />
+    </view>
+  )
   }
 }
  export default App;
